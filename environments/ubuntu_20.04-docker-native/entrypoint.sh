2dccc9e538577d6061636eeff091c27a3b7c9b00
Joe Walp <joewalp@gmail.com>
2024-12-16 17:31:37 -0600

    Add task descriptions

--- environments/ubuntu_20.04-docker-native/entrypoint.sh
+++ environments/ubuntu_20.04-docker-native/entrypoint.sh
@@ -3,17 +3,33 @@ set -o nounset -o pipefail -o errexit
 IFS=$'\t\n' # Stricter IFS settings
 rc=0
 
+# TASK [Define variables]
+
 INIT_FLAG_FILEPATH="${HOME}/.user-dir-initialized"
 
 if [[ ! -f "${INIT_FLAG_FILEPATH}" ]]; then
+
+  # TASK [Initializes the user's home directory in the /home volume`]
+
   rm -rf "${HOME}"
   mv "/entrypoint/${USER}" "${HOME}"
+
+  # TASK [Symlink the ~/.oh-my-zsh/custom directory to the mounted custom directory]
+  
   if [[ -d /entrypoint/oh-my-zsh-custom ]]; then
     rm -rf "${HOME}/.oh-my-zsh/custom"
     ln -s -T /entrypoint/oh-my-zsh-custom "${HOME}/.oh-my-zsh/custom"
   fi
+
+  # TASK [Signal that initialization is complete]
+  # 
+  # Note: External scripts poll for the existence of this flag file when
+  # waiting for initialization.
+  
   touch "${INIT_FLAG_FILEPATH}"
 fi
+
+# TASK [Exec the CMD or the specified command]
 
 exec "$@"
 
