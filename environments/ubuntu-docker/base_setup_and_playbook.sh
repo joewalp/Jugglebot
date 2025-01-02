4cd71cab7cc5afbfad259fcd46a12582807baf27
Joe Walp <joewalp@gmail.com>
2025-01-01 18:57:20 -0800

    Refactor the docker build script

--- environments/ubuntu-docker/base_setup_and_playbook.sh
+++ environments/ubuntu-docker/base_setup_and_playbook.sh
@@ -14,17 +14,7 @@ task 'Parse the arguments'
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -e|--environments-dir)
-      ENVIRONMENTS_DIR="$2"
-      shift
-      shift
-      ;;
-    -w|--ros-workspace-dir)
-      ROS_WORKSPACE_DIR="$2"
-      shift
-      shift
-      ;;
-    -p|--ansible-become-pass)
+    --ansible-become-pass)
       ANSIBLE_BECOME_PASS="$2"
       shift
       shift
@@ -40,26 +30,16 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-task 'Assert that the environments directory was specified'
-
-if [[ -z "${ENVIRONMENTS_DIR:-}" ]]; then
-  echo '[ERROR]: The environments directory is required. Invoke this command with the `--environments-dir "[directory path]"` option.' >&2
-  exit 2
-fi
-
-task 'Assert that the ROS workspace directory was specified'
-
-if [[ -z "${ROS_WORKSPACE_DIR:-}" ]]; then
-  echo '[ERROR]: The ROS workspace directory is required. Invoke this command with the `--ros-workspace-dir "[directory path]"` option.' >&2
-  exit 2
-fi
-
 task 'Assert that the Ansible become password was specified'
 
 if [[ -z "${ANSIBLE_BECOME_PASS:-}" ]]; then
   echo '[ERROR]: Ansible become password is required. Invoke this command with the `--ansible-become-pass "[cleartext password]"` option.' >&2
   exit 2
 fi
+
+task 'Initialize variables'
+
+JUGGLEBOT_REPO_DIR="${JUGGLEBOT_REPO_DIR:-${HOME}/Jugglebot}"
 
 task 'Configure base_setup.sh'
 
@@ -71,14 +51,15 @@ REFRESH_JUGGLEBOT_ENV_ENABLED='no'
 
 task 'Source base_setup.sh'
 
-source "${ENVIRONMENTS_DIR}/ubuntu-common/base_setup.sh"
+source "${JUGGLEBOT_REPO_DIR}/environments/ubuntu-common/base_setup.sh"
 
 task 'Run the playbook'
 
 ANSIBLE_LOCALHOST_WARNING=False \
   ANSIBLE_INVENTORY_UNPARSED_WARNING=False \
   ANSIBLE_BECOME_PASS="${ANSIBLE_BECOME_PASS}" \
-  ansible-playbook "${ENVIRONMENTS_DIR}/ubuntu-docker/main_playbook.yml" \
-    -e upgrade_software=yes \
-    -e "ros_workspace_dir=${ROS_WORKSPACE_DIR}"
+  ansible-playbook \
+    "${JUGGLEBOT_REPO_DIR}/environments/ubuntu-docker/main_playbook.yml" \
+    -e "upgrade_packages_enabled__='yes'" \
+    -e "ros_version_codename='${ROS_CODENAME}'"
 
